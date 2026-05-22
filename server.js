562fc81893f395595b636f37ddde13876c7f28e2
JosechuEntalpia <josejesusarboledasherranz@gmail.com>
2026-05-22 02:28:31 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API IEE funcionando");
+});
+
+app.post("/calcular", (req, res) => {
+  const { caudal, saltoTermico } = req.body;
+
+  const potencia = 1.2 * caudal * saltoTermico / 3600;
+
+  res.json({
+    caudal,
+    saltoTermico,
+    potencia_kw: Number(potencia.toFixed(2))
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`API funcionando en puerto ${PORT}`);
+});
